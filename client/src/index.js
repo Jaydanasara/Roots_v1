c20918df8648f5b1f873dc1137b2ca38e6e9a6e4
jaydanasara <jaydanasara@yahoo.com>
2020-09-22 16:23:50 -0400

    sevice worker added

--- client/src/index.js
+++ client/src/index.js
@@ -2,40 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
+import swDev from "./swDev";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store/Store";
 import { PersistGate } from 'redux-persist/integration/react'
-// import {createStore, applyMiddleware,compose} from "redux";
 
-// import rootReducer from "./store/reducers/rootReducer";
-// import thunk from "redux-thunk";
-// import {persistStore,persistReducer} from "redux-persist";
-// import storage from 'redux-persist/lib/storage'
-
-// const persistConfig = {
-//     key: 'root',
-//     storage,
-//   }
-//   const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-//   export default () => {
-//     let store = createStore(persistedReducer)
-//     let persistor = persistStore(store)
-//     return { store, persistor }
-//   }
-//   const middleware =[thunk]
-
-
-// const store = createStore(rootReducer, compose (applyMiddleware(...middleware),
-//     window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
-//     )
-//     );
 
 ReactDOM.render(<Provider store= {store}><PersistGate loading={null} persistor={persistor}>
     <App /></PersistGate> </Provider>, document.getElementById('root'));
 
+swDev();
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
---
+++ client/src/swDev.js
@@ -0,0 +1,7 @@
+export default function swDev(){
+
+    let swUrl =`${process.env.PUBLIC_URL}/serviceworker.js`
+    navigator.serviceWorker.register(swUrl).then((response)=>{
+        console.warn("response",response)
+    })
+}
