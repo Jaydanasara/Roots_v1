54ea3f2bb5a35a564840144671559fb2b5c1b35d
jaydanasara <jaydanasara@yahoo.com>
2020-11-13 19:35:17 -0500

    no longer using .env

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/storage";
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    apiKey:"AIzaSyCXP1zQ4p0yztONIzc8iu8zyaacW2Fynk4",
+    authDomain:"roots-6f3a0.firebaseapp.com",
+    databaseURL: "https://roots-6f3a0.firebaseio.com",
+    projectId: "roots-6f3a0",
+    storageBucket:"roots-6f3a0.appspot.com",
+    messagingSenderId:"16203055088",
   
 })
  export const storage = firebase.storage();
